c85c032682a38b8a34adf83ac0c63e49d6d46c21
Xelius08 <brokegamer008@gmail.com>
2024-12-08 07:06:28 +0600

    moved to dotfiles

--- colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c1c1c1", "#080807" },
-	[SchemeSel] = { "#c1c1c1", "#222325" },
-	[SchemeOut] = { "#c1c1c1", "#7F7F81" },
-};
